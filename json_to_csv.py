e584f9280dbec05c29d618dcf2a220351884b646
mukesh <mukesh@dataweave.in>
2014-04-22 12:27:56 +0530

    checked file from rajesh,avinash,sucheth
    1.rajesh i have added comment in ur file
    2.avinash and sucheth please add those thing to caluclator.py file

--- json_to_csv.py
+++ json_to_csv.py
@@ -27,3 +27,10 @@ def JsonToCsv(inputfile,outputfile):
 inputfile=sys.argv[1]
 outputfile=sys.argv[2]
 JsonToCsv(inputfile,outputfile)
+#rajesh
+#1.what will happen if json file contains object with different keys,so u can't depend upon only key of first line
+#if some value is comma separated then we must put those values inside double qoutes.
+#i have added flipkart json file that is converted into some csv ,but csv didn't contains all the fields
+#if u convert some json to csv then its vice versa should also exist i.e form ur csv file we should able to get exact same json file.
+#try urself with flipkart data
+#although this is just for getting familiarity with git but if every thing init is fine then in that case we can use those script into our pipeline also
